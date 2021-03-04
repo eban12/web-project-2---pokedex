fc6743874d2991d761d5005dfb5177766ef25c38
davenbezz <50683672+davenbezz@users.noreply.github.com>
2021-03-04 04:35:47 +0300

    I've defined colors

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,21 @@ const moves_tab = document.querySelector('.moves');
 const body = document.querySelector('body');
 const img_container = document.querySelector('.img_container');
 const search_input = document.querySelector(".search_bar input");
+
+
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+};
