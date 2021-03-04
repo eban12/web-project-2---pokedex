11bdc30981c283689f585654063f133455bb41bf
Eyob Alemu <it.eyob.alemu@gmail.com>
2021-03-04 04:10:40 +0300

    get a all pokemons added to api object

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,4 +4,10 @@ const api = {
         const res = await fetch(url);
         return await res.json();
     },
+
+    getAllPokemon: async () => {
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1118")
+        const pokes =  await res.json()
+        return pokes.results
+    }
 }
